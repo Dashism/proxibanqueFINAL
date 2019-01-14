85431c4819bac434bedd7fbf618ff75496e555f7
Ahmed <ahmed.belmouden@yahoo.fr>
2019-01-14 20:18:30 +0100

    DAD

--- src/main/java/fr/formation/proxibanqueFINAL/presentation/ViewController.java
+++ src/main/java/fr/formation/proxibanqueFINAL/presentation/ViewController.java
@@ -1,6 +1,7 @@
 package fr.formation.proxibanqueFINAL.presentation;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.log4j.Logger;
 import org.hibernate.Hibernate;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import fr.formation.proxibanqueFINAL.ProxibanqueFinalConstants;
 import fr.formation.proxibanqueFINAL.metier.ClientService;
@@ -89,8 +89,18 @@ public class ViewController {
 	}
 
 	@RequestMapping(path = "createsurvey", method = RequestMethod.POST)
-	public String validateForm(Survey survey, RedirectAttributes attributes ) {
-		String message = null;
+	public String validateForm(String beginDate, String supposedFinishDate ) {
+		final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate dt1 = LocalDate.parse(beginDate, dtf);
+		LocalDate dt2 = LocalDate.parse(supposedFinishDate, dtf);
+		
+		
+		//ModelAndView mav = new ModelAndView("createsurvey");
+		Survey survey = new Survey();
+		survey.setBeginDate(dt1);
+		survey.setSupposedFinishDate(dt2);
+		this.surveyService.create(survey);
+		return ProxibanqueFinalConstants.REDIRECT_TO_INDEX;
 		// Si l'identifiant est null alors on peut effectuer la création, et si
 		// la création
 		// renvoie vrai alors on met le message de succès, sinon on passe au
@@ -99,23 +109,20 @@ public class ViewController {
 		// l'URL, contrairement aux attributs de redirection normaux).
 		// Le message sera reçu par le nouveau paramètre "message" de la méthode
 		// index (ciblée par la redirection).
-		attributes.addFlashAttribute("message", message);
-		// On change pour un type de retour String permettant de renvoyer
-		// uniquement le nom de vue de redirection.
-		return ProxibanqueFinalConstants.REDIRECT_TO_INDEX;
+
 	}
 
 	@RequestMapping("listsurvey")
 	public ModelAndView listsurvey(Integer id) {
 		ModelAndView mav = new ModelAndView();
-		Survey survey = new Survey();
+		//Survey survey = new Survey();
 		// Il suffit d'ajouter la clé "author" au model pour que la valeur soit
 		// conservée en session (grâce à l'annotation sur la classe).
 		// 1. Configurer la vue.
 		mav.setViewName("listsurvey");
 
 		mav.addObject("surveys" , this.surveyService.readAll());
-		mav.addObject("percentages" , this.surveyService.getOpinionStats(survey));
+		//mav.addObject("percentages" , this.surveyService.getOpinionStats(survey));
 
 
 
